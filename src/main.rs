1e5894c79eca778fe13c0a3a803263840a2ac707
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2022-02-27 12:12:25 +0600

    Change from actix-web to tide

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,52 @@
 use std::env;
+use std::time::{ SystemTime, UNIX_EPOCH };
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use tide::{Request, Result, Body};
+use tide::prelude::{Deserialize, Serialize};
 
-#[get("/")]
-async fn greeting() -> impl Responder {
-	String::from("Hello, world!")
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+	id: Option<u128>,
+	name: String,
+	email: String,
+	password: String
 }
 
-#[get("/{name}")]
-async fn greeting_name(web::Path(name): web::Path<String>) -> impl Responder {
-	format!("Hello {}!", name)
+async fn greeting(_: Request<()>) -> Result {
+	Ok(String::from("Hello, world!").into())
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-	let mut port = String::from("3000");
-	
-	if let Ok(val) = env::var("PORT") {
-		port = val;
-	}
+async fn greeting_name(req: Request<()>) -> Result {
+	Ok(format!("Hello {}!", req.param("name").unwrap()).into())
+}
+
+async fn post_user(mut req: Request<()>) -> Result<Body> {
+	let user = req.body_json().await?;
+
+	Body::from_json(&User {		
+		id: Some(SystemTime::now()
+					.duration_since(UNIX_EPOCH)
+					.expect("Time went backwards")
+					.as_millis()),
+		..user
+	})
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+	let port = env::var("PORT").unwrap_or(String::from("3000"));
 
 	println!("Running on 0.0.0.0:{port}");
 
-	HttpServer::new(
-		|| App::new()
-			.service(greeting_name)
-			.service(greeting)
-	).bind(format!("0.0.0.0:{port}"))?
-		.run()
-		.await
+	let mut app = tide::new();
+
+	app.at("/").get(greeting);
+	app.at("/post").post(post_user);
+	app.at("/{name}").get(greeting_name);	
+
+	app.listen(format!("0.0.0.0:{port}")).await?;
+
+	Ok(())
 }
 
 
